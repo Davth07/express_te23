1fea68b9d8cfa14a74e202695927bace32c54245
David <david.thornqvist@elev.ga.ntig.se>
2026-04-01 11:23:41 +0200

    Added lesson on reading url values

--- server.js
+++ server.js
@@ -12,6 +12,28 @@ app.get("/users", (req, res) => {
     res.send("Here are the users")
 })
 
+app.get("/hello/:name", (req, res) =>{
+    res.send(`Hello, ${req.params.name}!`)
+})
+app.get("/search", (req, res) => {
+    console.log(req.query.q);
+    res.send(`You searched: ${req.query.q}`)
+})
+
+app.get("/filter", (req, res) => {
+    console.log(req.query);
+    res.send(`Category: ${req.query.sort}`)
+})
+
+app.get("/products/:id", (req, res) => {
+    console.log(req.params);
+    res.send(`You are looking for the product with id: ${req.params.id}`)
+})
+app.get("/products", (req, res) => {
+    console.log(req.params);
+    res.send(`You searced:`)
+})
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 })
